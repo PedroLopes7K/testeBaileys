50a82e85df3f47b6303ceb185693d09d357c1f8e
PedroLopes <pedrohenri3601@gmail.com>
2022-06-22 15:45:44 -0300

    Teste

---
+++ botzdg.js
@@ -0,0 +1,157 @@
+const makeWaSocket = require('@adiwajshing/baileys').default;
+const { delay, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion } = require('@adiwajshing/baileys');
+const P = require('pino');
+const { unlink, existsSync, mkdirSync, readFileSync, chownSync } = require('fs');
+const express = require('express');
+const { body, validationResult } = require('express-validator');
+const http = require('http');
+const port = process.env.PORT || 8080 ;
+const app = express();
+const server = http.createServer(app);
+const ZDGPath = './ZDGSessions/';
+const ZDGAuth = 'auth_info.json';
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+const ZDGUpdate = (ZDGsock) => {
+   ZDGsock.on('connection.update', ({ connection, lastDisconnect, qr }) => {
+
+      // bloco 1
+      if (qr){
+         console.log('© BOT-ZDG - Qrcode: ', qr);
+      };
+      // bloco 2
+      if (connection === 'close') {
+         const ZDGReconnect = lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut
+         if (ZDGReconnect) ZDGConnection()
+         console.log(`© BOT-ZDG - CONEXÃO FECHADA! RAZÃO: ` + DisconnectReason.loggedOut.toString());
+         // condicao para remover o arquivo de autenticação
+         if (ZDGReconnect === false) {
+            const removeAuth = ZDGPath + ZDGAuth
+            unlink(removeAuth, err => {
+               if (err) throw err
+            })
+         }
+      }
+      // bloco 3
+      if (connection === 'open'){
+         console.log('© BOT-ZDG -CONECTADO')
+      }
+   })
+}
+
+const ZDGConnection = async () => {
+   const { version } = await fetchLatestBaileysVersion()
+// se a pasta de sessoes não existir, ele cria   
+   if (!existsSync(ZDGPath)) {
+// recursive: Indicates whether parent folders should be created. If a folder was created, the path to the first created folder will be returned.
+      mkdirSync(ZDGPath, { recursive: true });
+   }
+   //                   analisar essa função com o leo 
+   const { saveState, state } = useSingleFileAuthState(ZDGPath + ZDGAuth)
+   // console.log(saveState)
+   // console.log(state)
+   const config = {
+      auth: state,
+      logger: P({ level: 'error' }),
+      printQRInTerminal: true,
+      version,
+      connectTimeoutMs: 60_000,
+      async getMessage(key) {
+         return { conversation: 'botzg' };
+      },
+   }
+   // console.log(config)
+   // analisar com leo
+   const ZDGsock = makeWaSocket(config);
+   // console.log(ZDGsock)
+   // função onde sera definido que ira acontecer quando o script rodar
+   ZDGUpdate(ZDGsock.ev);
+  // função aparentemente usada para salvar a autenticação 
+   ZDGsock.ev.on('creds.update', saveState);
+
+   const ZDGSendMessage = async (jid, msg) => {
+      await ZDGsock.presenceSubscribe(jid)
+      await delay(2000)
+      await ZDGsock.sendPresenceUpdate('composing', jid)
+      await delay(1500)
+      await ZDGsock.sendPresenceUpdate('paused', jid)
+      return await ZDGsock.sendMessage(jid, msg)
+   }
+
+   // Send message
+   app.post('/zdg-message', [
+      body('jid').notEmpty(),
+      body('message').notEmpty(),
+   ], async (req, res) => {
+      const errors = validationResult(req).formatWith(({
+      msg
+      }) => {
+      return msg;
+      });
+      if (!errors.isEmpty()) {
+      return res.status(422).json({
+         status: false,
+         message: errors.mapped()
+      });
+      }
+   
+      const jid = req.body.jid;
+      const numberDDI = jid.substr(0, 2);
+      const numberDDD = jid.substr(2, 2);
+      const numberUser = jid.substr(-8, 8);
+      const message = req.body.message;
+
+      if (numberDDI !== '55') {
+         ZDGSendMessage(jid, { text: message }).then(response => {
+            res.status(200).json({
+               status: true,
+               response: response
+            });
+            }).catch(err => {
+            res.status(500).json({
+               status: false,
+               response: err
+            });
+            });
+      }
+      if (numberDDI === '55' && numberDDD <= 30) {
+         const numberZDG = "55" + numberDDD + "9" + numberUser + "@s.whatsapp.net";
+         ZDGSendMessage(numberZDG, { text: message }).then(response => {
+            res.status(200).json({
+               status: true,
+               response: response
+            });
+            }).catch(err => {
+            res.status(500).json({
+               status: false,
+               response: err
+            });
+            });
+      }
+      if (numberDDI === '55' && numberDDD > 30) {
+         const numberZDG = "55" + numberDDD + numberUser + "@s.whatsapp.net";
+         ZDGSendMessage(numberZDG, { text: message }).then(response => {
+            res.status(200).json({
+               status: true,
+               response: response
+            });
+            }).catch(err => {
+            res.status(500).json({
+               status: false,
+               response: err
+            });
+            });
+      }
+
+   });
+
+}
+
+ZDGConnection()
+
+server.listen(port, function() {
+   console.log('© BOT-ZDG - Servidor rodando na porta: ' + port);
+ });
