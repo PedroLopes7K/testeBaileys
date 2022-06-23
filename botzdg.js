1d2af2648089e0a353a9bf080e8cbbd273389c6e
PedroLopes <pedrohenri3601@gmail.com>
2022-06-23 15:16:08 -0300

    Update layout menssage

--- botzdg.js
+++ botzdg.js
@@ -5,6 +5,8 @@ const {  existsSync, mkdirSync, readFileSync, chownSync, rm } = require('fs');
 const express = require('express');
 const { body, validationResult } = require('express-validator');
 const http = require('http');
+const { url } = require('inspector');
+const { text } = require('express');
 const port = process.env.PORT || 8000 ;
 const app = express();
 const server = http.createServer(app);
@@ -66,7 +68,7 @@ const ZDGConnection = async () => {
          return { conversation: 'botzg' };
       },
    }
-   console.log(config.auth)
+   console.log(config.auth.creds.me)
    // analisar com leo
    const ZDGsock = makeWaSocket(config);
    // console.log(ZDGsock)
@@ -75,19 +77,29 @@ const ZDGConnection = async () => {
   // função aparentemente usada para salvar a autenticação 
    ZDGsock.ev.on('creds.update', saveCreds);
 
-   const ZDGSendMessage = async (jid, msg) => {
+   const ZDGSendMessage = async (jid, templateMessage) => {
       await ZDGsock.presenceSubscribe(jid)
       await delay(2000)
       await ZDGsock.sendPresenceUpdate('composing', jid)
       await delay(1500)
       await ZDGsock.sendPresenceUpdate('paused', jid)
-      return await ZDGsock.sendMessage(jid, msg)
+
+   
+      return await ZDGsock.sendMessage(jid, templateMessage)
    }
 
    // Send message
    app.post('/zdg-message', [
       body('jid').notEmpty(),
       body('message').notEmpty(),
+      body('footer').notEmpty(),
+      body('id1').notEmpty(),
+      body('id2').notEmpty(),
+      body('id3').notEmpty(),
+      body('displaytext1').notEmpty(),
+      body('displaytext2').notEmpty(),
+      body('displaytext3').notEmpty(),
+      body('img').notEmpty(),
    ], async (req, res) => {
       const errors = validationResult(req).formatWith(({
       msg
@@ -105,10 +117,34 @@ const ZDGConnection = async () => {
       const numberDDI = jid.substr(0, 2);
       const numberDDD = jid.substr(2, 2);
       const numberUser = jid.substr(-8, 8);
+      const img = req.body.img
       const message = req.body.message;
+      const footer = req.body.footer;
+      const id1 = req.body.id1;
+      const id2 = req.body.id2;
+      const id3 = req.body.id3;
+      const displaytext1 = req.body.displaytext1;
+      const displaytext2 = req.body.displaytext2;
+      const displaytext3 = req.body.displaytext3;
+      const buttons = [
+         { buttonId: id1, buttonText: { displayText: displaytext1 }, type: 1 },
+         { buttonId: id2, buttonText: { displayText: displaytext2 }, type: 1 },
+         { buttonId: id3, buttonText: { displayText: displaytext3 }, type: 1 },
+      ]
+       
+       const templateMessage = {
+         //   image: {url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_PrTc4pn62Z_SdzMI-ZcOJ0afnoskVAo1rQ&usqp=CAU'},
+           image: {url: img},
+           caption: message,
+           footer: footer,
+           buttons: buttons,
+           headerType: 4
+       }
+
+
 
       if (numberDDI !== '55') {
-        await ZDGSendMessage(jid, { text: message }).then(response => {
+        await ZDGSendMessage(jid, {text: message}).then(response => {
             res.status(200).json({
                status: true,
                response: response
@@ -122,7 +158,7 @@ const ZDGConnection = async () => {
       }
       if (numberDDI === '55' && numberDDD <= 30) {
          const numberZDG = "55" + numberDDD + "9" + numberUser + "@s.whatsapp.net";
-        await ZDGSendMessage(numberZDG, { text: message }).then(response => {
+        await ZDGSendMessage(numberZDG, {text: message}).then(response => {
             res.status(200).json({
                status: true,
                response: response
@@ -136,7 +172,8 @@ const ZDGConnection = async () => {
       }
       if (numberDDI === '55' && numberDDD > 30) {
          const numberZDG = "55" + numberDDD + numberUser + "@s.whatsapp.net";
-       await  ZDGSendMessage(numberZDG, { text: message }).then(response => {
+       await  ZDGSendMessage(numberZDG, templateMessage).then(response => {
+         // console.log(response)
             res.status(200).json({
                status: true,
                response: response
